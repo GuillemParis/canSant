d82814d6b4c27b3538191c1b3cb7c86a3f0ce7e7
GuillemParis <guilemparis@gmail.com>
2017-04-03 16:23:42 +0200

    preparacio2

---
+++ data/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$("#inicioBTN").click(function(){
+		window.location.replace("./index.html");
+	});
+
+	$("#galeriaBTN").click(function(){
+		window.location.replace("./galeria.html");
+	});
+
+	$("#contactoBTN").click(function(){
+		window.location.replace("./contactes.html");
+	});
+});
