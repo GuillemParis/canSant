5fd836cb4341a635a24a21b4df04b1347f38f887
GuillemParis <guilemparis@gmail.com>
2017-04-03 16:40:12 +0200

    preparacio 3

--- data/js/maps.js
+++ data/js/maps.js
@@ -1,11 +1,16 @@
-function myMap() {
-	var center = new google.maps.LatLng(39.57250958154583,3.2014614364820773);
-	var mapProp= {
-	    center:center,
-	    zoom:19
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	var marker = new google.maps.Marker({position:center});
-	marker.setMap(map);
-	google.maps.event.trigger(map, 'resize');
-}
+$(document).ready(function(){
+
+	myMap();
+
+	function myMap() {
+		var center = new google.maps.LatLng(39.57250958154583,3.2014614364820773);
+		var mapProp= {
+		    center:center,
+		    zoom:19
+		};
+		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		var marker = new google.maps.Marker({position:center});
+		marker.setMap(map);
+		google.maps.event.trigger(map, 'resize');
+	}
+});
