42d115b808bca38d2bd70fd0ec5381edb190b0b0
mianv <miquelangel.vf@gmail.com>
2017-04-07 17:59:58 +0200

    carousel work

--- data/js/carousel.js
+++ data/js/carousel.js
@@ -8,8 +8,19 @@ $(document).ready(function() {
         success: function(result) {
             var imagenes = eval(result);
             for (var i = 2; i < imagenes.length; i++) {
-                $("#img-container").append('<div class="thumbnail" style="background-image: url(data/img/sales/' + imagenes[i] + ')"></div>')
+                $("#img-container").append('<div class="thumbnail" onclick="goToSlide(' + (i - 2) + ')" style="background-image: url(data/img/sales/' + imagenes[i] + ')"></div>');
+                if (i == 2){
+                  $(".carousel-inner").append('<div class="item active"><img src="data/img/sales/' + imagenes[i] + '" alt=""><div class="carousel-caption"></div></div>');
+                }else{
+                  $(".carousel-inner").append('<div class="item"><img src="data/img/sales/' + imagenes[i] + '" alt=""><div class="carousel-caption"></div></div>');
+                }
             }
         }
     });
+
 });
+
+function goToSlide(number) {
+  $("#myModal").modal("toggle");
+  $("#").carousel(number);
+}
